fb284e21dbb9f1eb83aeb7c4abd46600439b03cd
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-02-22 16:56:24 -0600

    Last Weekday Update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,27 @@ import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
 import Rank from './components/Rank/Rank.js';
+import Particles from 'react-particles-js';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+}
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className = 'App'>
+      <Particles className = 'particles'
+                params={particleOptions} />
         <Navigation />
         <Logo />
         <Rank />
