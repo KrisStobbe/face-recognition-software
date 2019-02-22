b57d7ab394b90b67104eaf2eca4216d21479c55a
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-02-22 16:27:47 -0600

    Added React Componenets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from './components/Navigation/Navigation.js'
+import Navigation from './components/Navigation/Navigation.js';
+import Logo from './components/Logo/Logo.js';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import Rank from './components/Rank/Rank.js';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navigation />
-        {/*<Logo />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*
         <ImageLinkForm />
         <FaceRecognition />*/}
       </div>
