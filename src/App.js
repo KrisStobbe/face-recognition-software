a7d65b974181584debc8788e23d3f414eda9261c
Kristoffer Stobbe <krisstobbe@yahoo.com>
2019-02-25 16:10:01 -0600

    Updating Image API

--- src/App.js
+++ src/App.js
@@ -30,23 +30,40 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: '',
+      box: {}
     }
   }
 
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return{
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+  }
+  console.log(image,height);
+}
+
+displayFaceBox = (box) => {
+  this.setState({box: box});
+}
+
 onInputChange = (event) => {
-    console.log(event)
+    this.setState({input: event.target.value});
   }
 
 onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input})
   console.log('click');
-  app.models.predict("a403429f2ddf4b49b307e318f00e528b",
-    "https://samples.clarifai.com/metro-north.jpg")
-    .then(function(response) {
-        console.log(response);
-        },
-        function(err) { // there was an error
-        }
-    );
+  app.models.predict(Clarifai.FACE_DETECT_MODEL,
+    this.state.input)
+    .then(response =>this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
 }
 
   render() {
@@ -58,7 +75,7 @@ onButtonSubmit = () => {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './FaceRecognition.css'
 
-const FaceRecognition = () => {
+const FaceRecognition = ({ imageUrl, box}) => {
+	console.log('check', imageUrl)
 	return (
-		<div className = 'center'>
-			<img src='https://samples.clarifai.com/metro-north.jpg' />
+		<div className = 'center imgUpdate'>
+			<img id='inputimage' src={imageUrl} width='auto' height='auto' />
+			<div className ='bounding-box'></div>
 		</div>
 	);
 }
